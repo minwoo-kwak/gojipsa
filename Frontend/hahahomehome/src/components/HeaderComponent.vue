d782f86debff9b0669945b1b7352a1969099611d
minwoo-kwak <minwoo.kwak540@gmail.com>
2023-11-06 17:43:12 +0900

    fix: korean decoding error in vuejs because of xss processing in backend

--- Frontend/hahahomehome/src/components/HeaderComponent.vue
+++ Frontend/hahahomehome/src/components/HeaderComponent.vue
@@ -3,47 +3,48 @@ import { storeToRefs } from "pinia";
 import { useUserStore } from "../stores/user";
 
 const userStore = useUserStore();
-const { menuList } = storeToRefs(userStore)
+// "userStore"에서 "menuList"를 가져와서 reactive reference로 사용.
+const { menuList } = storeToRefs(userStore);
 
-
-import { useRouter } from "vue-router";
-const router = useRouter();
-
+/**
+ * 로그아웃 함수
+ */
 function logout() {
-    const menuList = JSON.parse(sessionStorage.getItem('userStore'))['menuList']
+    // 세션 스토리지에서 "userStore"를 가져와서 "menuList" 추출
+    const menuList = JSON.parse(sessionStorage.getItem('userStore'))['menuList'];
 
-    // 메인 페이지로 이동합니다.
-    // router.push("/");
-    // 로그아웃 후 loginStatus를 토글합니다.
+    // 각 메뉴의 "loginStatus"를 토글.
     for (let menu of menuList.value) {
         menu.loginStatus = !menu.loginStatus;
     }
-    // 서버에서 로그아웃 성공 응답을 받으면 로컬 스토리지의 토큰을 삭제합니다.
+    
+    // 서버에서 로그아웃 성공 응답을 받으면 로컬 스토리지의 토큰을 삭제.
     sessionStorage.removeItem("userStore");
- 
 }
 </script>
 
 <template>
     <v-app-bar>
         <v-app-bar-title text="HaHaHomeHome" class="v-col-3" @click="$router.push('/')"></v-app-bar-title>
-            <v-list class="v-col-8">
-                <v-row class="justify-end">
-                    <template v-for="menu in menuList">
-                        <list-item v-if="menu.loginStatus" :key="menu.name">
-                            <template v-if="menu.name == '로그아웃'">
-                                <router-link to="/" @click="logout()">{{ menu.name }}</router-link>
-                            </template>
-                            <template v-else>
-                                <router-link :to="{name: menu.routeName}">{{ menu.name }}</router-link>
-                            </template>
-                        </list-item>
-                    </template>
-                </v-row>
-            </v-list>
+        <v-list class="v-col-8">
+            <v-row class="justify-end">
+                <!-- "menuList"에 대한 루프를 수행하여 메뉴 항목을 생성합니다. -->
+                <template v-for="menu in menuList">
+                    <list-item v-if="menu.loginStatus" :key="menu.name">
+                        <!-- "menu.name"이 '로그아웃'인 경우 로그아웃 링크를 생성합니다. -->
+                        <template v-if="menu.name == '로그아웃'">
+                            <router-link to="/" @click:prevent="logout()">{{ menu.name }}</router-link>
+                        </template>
+                        <!-- 그 외 메뉴 항목은 해당 routeName으로 라우팅 링크를 생성합니다. -->
+                        <template v-else>
+                            <router-link :to="{name: menu.routeName}">{{ menu.name }}</router-link>
+                        </template>
+                    </list-item>
+                </template>
+            </v-row>
+        </v-list>
     </v-app-bar>
 </template>
 
 <style scoped>
-
 </style>
--- Frontend/hahahomehome/src/stores/user.js
+++ Frontend/hahahomehome/src/stores/user.js
@@ -1,33 +1,39 @@
 import { ref, inject } from 'vue'
 import { defineStore } from "pinia"
 
+// useUserStore 스토어 정의
 export const useUserStore = defineStore('userStore', () => {
-    // main.js 에서 app.provie한 객체 가져오기
-    const axios = inject('axios')
-    const Authorization = ref(null)   
+    // main.js 에서 app.provide로 주입된 axios 객체를 가져옵니다.
+    const axios = inject('axios');
+
+    // Authorization 및 menuList를 ref로 생성합니다.
+    const Authorization = ref(null);
     const menuList = ref([
         { name: '로그인', loginStatus: true, routeName: 'login' },
         { name: '회원가입', loginStatus: true, routeName: 'signup' },
         { name: '게시판', loginStatus: false, routeName: 'login' },
         { name: '마이페이지', loginStatus: false, routeName: 'mypage' },
         { name: '로그아웃', loginStatus: false, routeName: 'login' },
-    ])
+    ]);
 
+    // 로그인 함수 정의
     async function login(userId, password) {
+        // axios를 사용하여 '/user/login' 엔드포인트에 POST 요청을 보냅니다.
         await axios.post('/user/login', { userId, password })
             .then((response) => {
                 Authorization.value = response.data.Authorization;
-                console.log(Authorization.value)
-            })
+                console.log(Authorization.value);
+            });
         
         // 로그인 성공시
         if (Authorization.value !== null) {
+            // axios의 기본 헤더에 Authorization 토큰을 설정합니다.
             axios.defaults.headers.common['Authorization'] = Authorization.value;
 
-            // 토글
+            // 메뉴 항목의 로그인 상태를 토글합니다.
             menuList.value.forEach(element => {
                 element.loginStatus = !element.loginStatus;
-            })
+            });
         }
     }
 
@@ -37,8 +43,9 @@ export const useUserStore = defineStore('userStore', () => {
         login
     }
 },
-    {
-        persist: {
-        storage: sessionStorage
+{
+    // 상태 유지 설정
+    persist: {
+        storage: sessionStorage // 세션 스토리지를 사용하여 상태를 유지합니다.
     }
 })
