aef5ec8a541634a1bc64bdade1fe128072715a6d
hayeooo <khy3496@cau.ac.kr>
2023-11-07 12:14:05 +0900

    fix : debug xss filter

--- Backend/hahahomehome/src/main/java/com/ssafy/house/filter/HTMLCharacterExcapes.java
+++ Backend/hahahomehome/src/main/java/com/ssafy/house/filter/HTMLCharacterExcapes.java
@@ -1,6 +1,8 @@
 package com.ssafy.house.filter;
 
-import org.apache.commons.lang.StringEscapeUtils;
+import java.util.Arrays;
+
+import org.apache.commons.text.StringEscapeUtils;
 
 import com.fasterxml.jackson.core.SerializableString;
 import com.fasterxml.jackson.core.io.CharacterEscapes;
@@ -16,14 +18,13 @@ public class HTMLCharacterExcapes extends CharacterEscapes{
 	public HTMLCharacterExcapes() {
 		// XSS 방지 처리할 특수 문자 지정
 		asciiEscapes=CharacterEscapes.standardAsciiEscapesForJSON();
+		System.out.println(CharacterEscapes.ESCAPE_CUSTOM);
 		asciiEscapes['<']=CharacterEscapes.ESCAPE_CUSTOM;
 		asciiEscapes['>']=CharacterEscapes.ESCAPE_CUSTOM;
 		asciiEscapes['&']=CharacterEscapes.ESCAPE_CUSTOM;
-		asciiEscapes['\"']=CharacterEscapes.ESCAPE_CUSTOM;
 		asciiEscapes['(']=CharacterEscapes.ESCAPE_CUSTOM;
 		asciiEscapes[')']=CharacterEscapes.ESCAPE_CUSTOM;
 		asciiEscapes['#']=CharacterEscapes.ESCAPE_CUSTOM;
-		asciiEscapes['\'']=CharacterEscapes.ESCAPE_CUSTOM;
 	}
 	@Override
 	public int[] getEscapeCodesForAscii() {
@@ -32,7 +33,7 @@ public class HTMLCharacterExcapes extends CharacterEscapes{
 
 	@Override
 	public SerializableString getEscapeSequence(int ch) {
-		return new SerializedString(StringEscapeUtils.escapeHtml(Character.toString((char) ch)));
+		return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char)ch)));
 	}
 
 }
