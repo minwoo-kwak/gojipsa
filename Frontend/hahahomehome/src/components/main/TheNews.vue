25feebd5f8f622a26cee1d704eb8084ee3ab5786
minwoo-kwak <minwoo.kwak540@gmail.com>
2023-11-09 17:15:23 +0900

    feat: add notice modal and news link

--- Frontend/hahahomehome/src/components/main/TheNews.vue
+++ Frontend/hahahomehome/src/components/main/TheNews.vue
@@ -7,27 +7,38 @@
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">제목</th>
+          <th scope="col">언론사</th>
+          <th scope="col">날짜</th>
+          <th scope="col">조회수</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
+          <td>
+            <a
+              href="https://www.mk.co.kr/news/realestate/10870756"
+              target="_blank"
+              rel="noopener noreferrer"
+              >뉴스 제목입니다.</a
+            >
+          </td>
+          <td>매일경제</td>
+          <td>2023-11-09 17:02:41</td>
           <td>@mdo</td>
         </tr>
         <tr>
           <th scope="row">2</th>
           <td>Jacob</td>
           <td>Thornton</td>
+          <td>2023-11-09 17:02:41</td>
           <td>@fat</td>
         </tr>
         <tr>
           <th scope="row">3</th>
           <td colspan="2">Larry the Bird</td>
+          <td>2023-11-09 17:02:41</td>
           <td>@twitter</td>
         </tr>
       </tbody>
--- Frontend/hahahomehome/src/components/user/LoginComponent.vue
+++ Frontend/hahahomehome/src/components/user/LoginComponent.vue
@@ -1,5 +1,4 @@
 <script setup>
-import axios from 'axios'
 import { ref } from 'vue'
 import { useUserStore } from '../../stores/user'
 import { useRouter } from 'vue-router'
