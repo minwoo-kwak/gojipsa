979fa68e300036cd7bdb796229120e463c22fb62
hayeooo <khy3496@cau.ac.kr>
2023-11-09 15:30:24 +0900

    feat: add kakao map

---
+++ Frontend/hahahomehome/src/components/apart/TheApartList.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="apart-info"></div>
+</template>
+
+<style scoped lang="scss">
+.apart-info {
+  width: 30rem;
+}
+</style>
---
+++ Frontend/hahahomehome/src/views/ApartView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import KakaoMap from '../components/apart/KakaoMap.vue'
+import TheApartList from '../components/apart/TheApartList.vue'
+</script>
+
+<template>
+  <TheApartList />
+  <KakaoMap />
+</template>
+
+<style scoped></style>
