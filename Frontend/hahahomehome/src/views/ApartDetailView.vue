29289313ebc2ac7b4b9e67ace5a7ad36b44792a1
minwoo-kwak <minwoo.kwak540@gmail.com>
2023-11-14 17:46:26 +0900

    init: kakao map detail view

---
+++ Frontend/hahahomehome/src/views/ApartDetailView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheHeading from '../components/common/TheHeading.vue'
+import KakaoDetailMap from '../components/apart/KakaoDetailMap.vue'
+</script>
+
+<template>
+  <TheHeading />
+  <div class="apart-detail-view">
+    <TheApartList />
+    <KakaoDetailMap />
+  </div>
+</template>
+
+<style scoped>
+.apart-detail-view {
+  display: flex;
+  padding-top: 65px;
+}
+</style>
