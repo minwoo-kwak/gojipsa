5fa55340d1af9e9124785a46c782a3418160d2bf
hayeooo <khy3496@cau.ac.kr>
2023-11-17 17:28:29 +0900

    feat: checklist pop up with apartcode

--- Frontend/hahahomehome/src/components/apart/TheCheckList.vue
+++ Frontend/hahahomehome/src/components/apart/TheCheckList.vue
@@ -1,6 +1,9 @@
 <script setup>
+const props = defineProps({
+  aptcode: String
+})
 const showCheckList = () => {
-  window.open('/apart/detail/checklist', '_blank', 'width=700, height=720')
+  window.open(`/apart/detail/checklist/${props.aptcode}`, '_blank', 'width=700, height=720')
 }
 </script>
 
