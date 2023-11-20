2e3fdda58480b9777867462309283ef7755b304c
hayeooo <khy3496@cau.ac.kr>
2023-11-20 15:55:17 +0900

    feat : add logo png

--- Frontend/hahahomehome/src/components/common/TheHeading.vue
+++ Frontend/hahahomehome/src/components/common/TheHeading.vue
@@ -19,7 +19,7 @@ async function logout() {
 
 <template>
   <v-app-bar>
-    <v-app-bar-title text="Go!집사" class="v-col-1" @click="$router.push('/')"></v-app-bar-title>
+    <v-app-bar-title text="Go!집사" class="v-col-1" @click="$router.push('/')"> </v-app-bar-title>
     <v-list class="v-col-3">
       <router-link :to="{ name: 'apart', params: { code: dongcode } }">실거래가 확인</router-link>
       <router-link :to="{ name: 'analysis' }">고집사의 경제분석실</router-link>
