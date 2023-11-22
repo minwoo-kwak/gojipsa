1396af3044903714be0c69f904ad3daf1a26d6d2
minwoo-kwak <minwoo.kwak540@gmail.com>
2023-11-22 12:41:27 +0900

    rename: project

--- Backend/GOJIPSA/src/main/java/com/ssafy/house/GojipsaApplication.java
+++ Backend/GOJIPSA/src/main/java/com/ssafy/house/GojipsaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HahahomehomeApplication {
+public class GojipsaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HahahomehomeApplication.class, args);
+		SpringApplication.run(GojipsaApplication.class, args);
 	}
 
 }
